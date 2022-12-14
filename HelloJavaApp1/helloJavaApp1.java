c840b9175827cb5c58a9e43b285cb4cc04bb778f
Darshan Patel <dpatel@sgi.sk.ca>
2022-12-14 11:10:56 -0600

    darshan test1

---
+++ HelloJavaApp1/helloJavaApp1.java
@@ -0,0 +1,12 @@
+package HelloJavaApp1;
+
+/**
+ * helloJavaApp1
+ */
+public class helloJavaApp1 {
+
+    public static void main(String[] args) {
+        System.out.println("This is my first Java Code");
+    }
+
+}
